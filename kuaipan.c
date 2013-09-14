bec917c503d1c3a4669e022bac1607754b1779ad
shadowlaser <yang3911@126.com>
2013-09-14 09:50:04 +0800

    代码命名调整和注释添加

--- kuaipan.c
+++ kuaipan.c
@@ -7,73 +7,70 @@
 #include "hmac_sha1.h"
 #include "kuaipan.h"
 
-extern int URLEncode(
-  const char* str, 
-  const int strSize, 
-  char* result, 
-  const int resultSize
-);
-extern char* base64_encode(const char* data, int data_len); 
-extern char* hmac_sha(
-  char* k, /* secret key */
-  int lk, /* length of the key in bytes */
-  char* d, /* data */
-  int ld, /* length of data in bytes */
-  char* out, /* output buffer, at least "t" bytes */
-  int t
-);
+#define MAX_LEN 1024
+
 int get_signature(char* http_url,char* oauth_consumer_key,char* oauth_nonce, char* oauth_signature_method,char* oauth_timestamp, char* oauth_version,char* consumer_secret,char oauth_signature[])
 {
-  //char* oauth_signature=(char*)malloc(1024*sizeof(char));
-  char obj[1024]="",d[1024]="",out[1024]="";
-  int lk=0,ld=0,t=0;
-  //char url[1024]="";
-  char consumer_key[1024]="";
-  char nonce[1024]="";	 
-  char signature_method[1024]="";  
-  char timestamp[1024]="";
-  char version[1024]="";
-  //sprintf(url,"%s&
-  sprintf(consumer_key,"oauth_consumer_key=%s",oauth_consumer_key);
-  sprintf(nonce,"&oauth_nonce=");
-  sprintf(signature_method,"&oauth_signature_method=%s",oauth_signature_method);
-  sprintf(timestamp,"&oauth_timestamp=%s",oauth_timestamp);
-  sprintf(version,"&oauth_version=%s",oauth_version);
-  
-  strcat(d,"GET&");
-  //printf("src=%s\n",d);
-  //strcat(d,"&",1);
-  URLEncode(http_url,strlen(http_url),obj,1024);
-  strcat(d,obj);
-  //printf("src=%s\n",d);
-  strcat(d,"&");
-  URLEncode(consumer_key,strlen(consumer_key),obj,1024);
-  strcat(d,obj);
-  //printf("src=%s\n",d);
-  URLEncode(nonce,strlen(nonce),obj,1024);
-  strcat(d,obj);
-  strcat(d,oauth_nonce);
-  //printf("src=%s\n",d);
-  URLEncode(signature_method,strlen(signature_method),obj,1024);
-  strcat(d,obj);
-  //printf("src=%s\n",d);
-  URLEncode(timestamp,strlen(timestamp),obj,1024);
-  strcat(d,obj);
-  URLEncode(version,strlen(version),obj,1024);
-  strcat(d,obj);
-  printf("src=%s\n%d\n",d,strlen(d));
-  ld=strlen(d);
-  lk=strlen(consumer_secret);
-  char *hmac_shastr=(char*)hmac_sha(consumer_secret,lk,d,ld,out,20);
-  int hmac_shalen=strlen(hmac_shastr);
-  printf("hmac_shastr=%s\n",hmac_shastr);
-  int base64_encodelen=strlen((char*)base64_encode(hmac_shastr, hmac_shalen));
-  URLEncode(base64_encode(hmac_shastr,hmac_shalen),base64_encodelen,obj,1024);
-  strcat(oauth_signature,obj);
-  //strcat(oauth_signature,"\0");
-  printf("%s\n%d\n",oauth_signature,strlen(oauth_signature));
-  return 1;
-  //return oauth_signature;
+    //char* oauth_signature=(char*)malloc(MAX_LEN*sizeof(char));
+    char tmp_result[MAX_LEN]="",request_url[MAX_LEN]="",out[MAX_LEN]="";
+    int t=0;
+    //char url[MAX_LEN]="";
+    char consumer_key[MAX_LEN]="";
+    char nonce[MAX_LEN]="";
+    char signature_method[MAX_LEN]="";
+    char time_stamp[MAX_LEN]="";
+    char version[MAX_LEN]="";
+    //sprintf(url,"%s&
+    sprintf(consumer_key,"oauth_consumer_key=%s",oauth_consumer_key);
+    sprintf(nonce,"&oauth_nonce=");
+    sprintf(signature_method,"&oauth_signature_method=%s",oauth_signature_method);
+    sprintf(time_stamp,"&oauth_timestamp=%s",oauth_timestamp);
+    sprintf(version,"&oauth_version=%s",oauth_version);
+
+    strcat(request_url,"GET&");
+    //http url encode
+    URLEncode(http_url,strlen(http_url),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+    strcat(request_url,"&");
+    
+    //consumer_key encode
+    URLEncode(consumer_key,strlen(consumer_key),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+    
+    //nonce encode
+    URLEncode(nonce,strlen(nonce),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+    strcat(request_url,oauth_nonce);
+    
+    //signature_method encode
+    URLEncode(signature_method,strlen(signature_method),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+    
+    //time_stamp encode
+    URLEncode(time_stamp,strlen(time_stamp),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+    
+    //oauth encode
+    URLEncode(version,strlen(version),tmp_result,MAX_LEN);
+    strcat(request_url,tmp_result);
+
+    int request_url_len=strlen(request_url);
+    printf("encode url = %s,encode length = %d\n",request_url,request_url_len);
+
+    int consumer_secret_len=strlen(consumer_secret);
+    //sha encrypt
+    char *hmac_sha_str=(char*)hmac_sha(consumer_secret,consumer_secret_len,request_url,request_url_len,out,20);
+    int hmac_sha_str_len=strlen(hmac_sha_str);
+    printf("hmac_sha_str = %s, hmac_sha_str_len = %d\n",hmac_sha_str, hmac_sha_str_len);
+
+    //base64 encode
+    char* base64_encode_result = base64_encode(hmac_sha_str, hmac_sha_str_len);
+    int base64_encode_result_len=strlen(base64_encode_result);
+    URLEncode(base64_encode_result,base64_encode_result_len,tmp_result,MAX_LEN);
+    strcat(oauth_signature,tmp_result);
+
+    printf("oauth_signature = %s, oauth_signature_len = %d\n",oauth_signature,strlen(oauth_signature));
+    return 1;
 }
 
 
